9f4d4545fc8b79d56719809bbc70b4bbb1643306
F640 <92260436+F640@users.noreply.github.com>
2025-08-26 17:46:50 +0700

    change script versioning method and make workflow shorter

--- generate.sh
+++ generate.sh
@@ -20,6 +20,10 @@ USERSCRIPT_MIN_UPDATE_URL="https:\/\/raw.githubusercontent.com\/F640\/qsniyg-max
 USERSCRIPT_DOWNLOAD_URL="https:\/\/raw.githubusercontent.com\/F640\/qsniyg-maxurl\/dist\/maxurl.user.js"
 USERSCRIPT_MIN_DOWNLOAD_URL="https:\/\/raw.githubusercontent.com\/F640\/qsniyg-maxurl\/dist\/maxurl_min.user.js"
 
+# create versioning helper variables file if does not exist
+mkdir -p dist && touch dist/versioner.txt
+VERSIONER_TXT_LOC='dist/versioner.txt'
+
 # check for git existence
 if ! command -v $GIT_BIN_CMD >/dev/null 2>&1
 then
@@ -37,10 +41,11 @@ fi
 # start cloning repo
 $GIT_BIN_CMD clone --depth 1 "https://github.com/qsniyg/maxurl.git"
 
+# fetch version
 cd maxurl
 REPO_COMMIT=$($GIT_BIN_CMD rev-parse HEAD)
 REPO_SHORT_COMMIT=$($GIT_BIN_CMD rev-parse --short HEAD)
-echo "creating script version commit $REPO_COMMIT"
+echo "current upstream commit: $REPO_COMMIT"
 cd ..
 
 # check userscript meta file existence
@@ -58,6 +63,31 @@ fi
 # make temp directory so it won't be messy
 mkdir tempo2123
 
+# make version stuff
+. dist/versioner.txt
+# get upstream version
+IMU_ORIG_VER=$(grep -Po -m 1 '^\/\/\s*@version\s*\K.*$' maxurl/src/userscript.ts | head -1)
+if [ -z "${IMU_ORIG_VER}" ]; then
+    echo "version is empty in userscript source file, either meta location changed or source has moved, getting it from userscript meta..."
+    IMU_ORIG_VER=$(grep -Po -m 1 '^\/\/\s*@version\s*\K.*$' maxurl/userscript.meta.js | head -1)
+    if [ -z "${IMU_ORIG_VER}" ]; then
+        echo "version not found, will use commit versioning instead (auto-updates will not work properly)"
+    else
+        IMU_CUR_VER=main-$REPO_SHORT_COMMIT
+    fi
+fi
+# compare vanilla upstream with current fork if version is found in source
+if [ "$IMU_ORIG_VER" != "$IMU_MOD_VER" ]; then
+    echo "upstream version has changed"
+    IMU_MOD_VER=$IMU_ORIG_VER
+    IMU_MOD_COUNTER=1
+    IMU_CUR_VER=$IMU_MOD_VER
+    IMU_CUR_VER="${IMU_MOD_VER}dev${IMU_MOD_COUNTER}"
+elif [ "$IMU_ORIG_VER" = "$IMU_MOD_VER" ]; then
+    IMU_MOD_COUNTER=$((IMU_MOD_COUNTER+1))
+    IMU_CUR_VER="${IMU_MOD_VER}dev${IMU_MOD_COUNTER}"
+fi
+
 # change userscript name
 echo 'changing script name...'
 sed -E "s/(\/\/([[:space:]]+)?@name:?([a-z,A-Z]{2}(-[a-z,A-Z]{2})?)?([[:space:]]+))(.+)/\1Image Max URL (unofficial dev version)/gm" $USERSCRIPT_META_LOCATION > tempo2123/userscript1.meta.js
@@ -65,8 +95,8 @@ sed -E "s/(\/\/([[:space:]]+)?@name:?([a-z,A-Z]{2}(-[a-z,A-Z]{2})?)?([[:space:]]
 
 # change userscript version
 echo 'changing script version...'
-sed -E "s/(\/\/([[:space:]]+)?@version([[:space:]]+))(.+)/\1main-$REPO_SHORT_COMMIT/gm" tempo2123/userscript1.meta.js > tempo2123/userscript2.meta.js
-sed -E "s/(\/\/([[:space:]]+)?@version([[:space:]]+))(.+)/\1main-$REPO_SHORT_COMMIT/gm" tempo2123/userscript1.ts > tempo2123/userscript2.ts
+sed -E "s/(\/\/([[:space:]]+)?@version([[:space:]]+))(.+)/\1$IMU_CUR_VER/gm" tempo2123/userscript1.meta.js > tempo2123/userscript2.meta.js
+sed -E "s/(\/\/([[:space:]]+)?@version([[:space:]]+))(.+)/\1$IMU_CUR_VER/gm" tempo2123/userscript1.ts > tempo2123/userscript2.ts
 
 # change userscript updater stuff
 echo 'changing script update metadata...'
@@ -117,3 +147,7 @@ cp tempo2123/userscript_smol2.user.js maxurl_min.user.js
 cp maxurl/userscript.meta.js maxurl.meta.js
 # metadata for minified one
 cp tempo2123/userscript_smol2.meta.js maxurl_min.meta.js
+
+# export variables
+grep -q '^IMU_MOD_VER=.*' $VERSIONER_TXT_LOC && sed -i "s/^IMU_MOD_VER=.*/IMU_MOD_VER=$IMU_MOD_VER/" $VERSIONER_TXT_LOC || echo "IMU_MOD_VER=$IMU_MOD_VER" >> $VERSIONER_TXT_LOC
+grep -q '^IMU_MOD_COUNTER=.*' $VERSIONER_TXT_LOC && sed -i "s/^IMU_MOD_COUNTER=.*/IMU_MOD_COUNTER=$IMU_MOD_COUNTER/" $VERSIONER_TXT_LOC || echo "IMU_MOD_COUNTER=$IMU_MOD_COUNTER" >> $VERSIONER_TXT_LOC
